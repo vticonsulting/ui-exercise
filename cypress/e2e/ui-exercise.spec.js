4384ec3276dafafe6984dbc082c9406ba17afdd7
Victor Tolbert <victor.tolbert@gmail.com>
2020-10-19 12:28:25 -0400

    try one more time

--- cypress/e2e/ui-exercise.spec.js
+++ cypress/e2e/ui-exercise.spec.js
@@ -2,10 +2,6 @@ describe('UI Exercise', () => {
   it('can display messages tagged as "work"', () => {
     cy.visit('/')
   })
-
-  it('can display messages tagged as "travel"', () => {
-    cy.visit('/').get('a.travel')
-  })
 })
 
 // findBy
